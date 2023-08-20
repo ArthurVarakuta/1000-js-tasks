0c76cb92c35cc0376186de21f1ab3e9eecab182d
ArthurVarakuta <varakutych@gmail.com>
2023-08-20 17:33:11 +0300

    mistakes were made......

---
+++ 3.js
@@ -0,0 +1,30 @@
+let date_holder = document.getElementById("dd_mm_yy");
+let date = new Date();
+let dd = date.getDate();
+month = date.getMonth()+1;//+1 corrects month value bcs 0=january 11=december
+day = date.getDate();
+
+function single_digit_month_handler(month)
+{
+    if (month < 10)
+    {
+        return "0" + month.toString();
+    } else
+    {
+        return month.toString();
+    }
+}
+
+function single_digit_day_handler(day) {
+    if (day < 10)
+    {
+        return "0" + day.toString();
+    } else
+    {
+        return day.toString();
+    }
+}
+
+console.log(date.getDate());
+date_holder.innerHTML = single_digit_day_handler(day) + '.' + single_digit_month_handler(month) + '.' + date.getFullYear();
+console.log(date.getDate());
