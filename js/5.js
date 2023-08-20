285ffaae529d157c12e903e177d1cea6f7432068
ArthurVarakuta <varakutych@gmail.com>
2023-08-20 23:34:52 +0300

    complete 5/1000 tasks

--- js/5.js
+++ js/5.js
@@ -1,8 +1,6 @@
 let reverted_word= document.getElementById("reverted_word_task5");
-word = "w3resource"
+word = "w3resource";
+const arr =[];
+for (let char of word) {
 
-for(char of word){
-   let word_array = Array.from(char);
-   word_array.reverse();
-   // console.log(word_array);
 }
