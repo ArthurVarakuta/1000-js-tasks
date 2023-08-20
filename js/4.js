cdb8e2f07f5496fe9937e1cd338534215877bcf0
ArthurVarakuta <varakutych@gmail.com>
2023-08-20 22:56:06 +0300

    refactored task4

--- js/4.js
+++ js/4.js
@@ -1,17 +1,45 @@
-a_side = document.getElementById("a_side_task4");
-b_side = document.getElementById("b_side_task4");
-c_side = document.getElementById("c_side_task4");
-result_task4 = document.getElementById("result_task4");
+a_map = document.getElementsByClassName("a_side_task4");
+b_map = document.getElementsByClassName("b_side_task4");
+c_map = document.getElementsByClassName("c_side_task4");
+result_map = document.getElementsByClassName("result_task4");
 btn = document.getElementById("btn_task4");
 
-function get_input_params() {
-  a=parseInt(a_side.value);
-  b=parseInt(b_side.value);
-  c=parseInt(c_side.value);
-  console.log(a, b, c);
+
+btn.addEventListener("click", function () {
+    listener();
+});
+
+function listener() {
+    get_all_triangle_results_from_map(triangles_map_array_maker(a_map, b_map, c_map, result_map));
 }
 
-function calculate_area(a, b, c) {
+
+function get_single_triangle_result(a, b, c, result) {
+    result.innerHTML = calculate_triangle_area(parseInt(a.value), parseInt(b.value), parseInt(c.value));
+    // console.log(parseInt(a.value), parseInt(b.value), parseInt(c.value));
+}
+
+function triangles_map_array_maker(a_map, b_map, c_map, result_map) {
+    let triangles = [];
+    for (let i = 0; i < a_map.length; i++) {
+        let map = new Map();
+        map.set(0, a_map.item(i));
+        map.set(1, b_map.item(i));
+        map.set(2, c_map.item(i));
+        map.set(3, result_map.item(i));
+        triangles.push(map);
+    }
+    return triangles;
+}
+
+function get_all_triangle_results_from_map(triangles) {
+    // let triangle = new Map();
+    triangles.forEach(function (triangle, i,triangles ){
+        get_single_triangle_result(triangle.get(0), triangle.get(1), triangle.get(2), triangle.get(3));
+      })
+    }
+
+function calculate_triangle_area(a, b, c) {
     if (a > 0 && b > 0 && c > 0) {
         let s = (a + b + c) / 2;
         return Math.sqrt(s * (s - a) * (s - b) * (s - c));
@@ -19,8 +47,3 @@ function calculate_area(a, b, c) {
         alert("Please enter positive numbers");
     }
 }
-
-btn.addEventListener("click", function () {
-    get_input_params();
-    result_task4.innerHTML = calculate_area(a, b, c);
-})
---
+++ js/5.js
@@ -0,0 +1,8 @@
+let reverted_word= document.getElementById("reverted_word_task5");
+word = "w3resource"
+
+for(char of word){
+   let word_array = Array.from(char);
+   word_array.reverse();
+   // console.log(word_array);
+}
