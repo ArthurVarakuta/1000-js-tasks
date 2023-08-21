0a8ab042afac1e63eab5a10854ad4a9b73686df4
ArthurVarakuta <varakutych@gmail.com>
2023-08-21 12:15:31 +0300

    refactor 5 task

--- js/5.js
+++ js/5.js
@@ -1,6 +1,24 @@
-let reverted_word= document.getElementById("reverted_word_task5");
-word = "w3resource";
-const arr =[];
-for (let char of word) {
+// let reverted_word = document.getElementById("reverted_word_task5");
+// word = "w3resource";
+// reverted_word.innerHTML = Array.from(word).reverse();
 
+//now harder
+
+let input = document.getElementById('word_input-task5');
+let output = document.getElementById('output_reversed_word_task5');
+let btn_task5 = document.getElementById('reverse_button_task5');
+
+btn_task5.addEventListener('click', function () {
+    get_input_text();
+    input_text_reverter();
+
+})
+
+function get_input_text(input_text) {
+    input_text= input.value;
+    return input_text;
+}
+
+function input_text_reverter() {
+    output.innerHTML  = Array.from(get_input_text()).reverse();
 }
